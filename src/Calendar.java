025e8e95fbaa28150b87478daa4d7abbcf69805e
kjw08100 <tirkffus@naver.com>
2021-02-09 14:28:48 +0900

    fix encoding problem

--- src/Calendar.java
+++ src/Calendar.java
@@ -2,7 +2,7 @@
 public class Calendar {
 
 	public static void main(String[] args) {
-		System.out.println("ÀÏ ¿ù È­ ¼ö ¸ñ ±İ Åä ");
+		System.out.println("ì¼ ì›” í™” ìˆ˜ ëª© ê¸ˆ í†  ");
 		System.out.println("1 2 3 4 5 6 7");
 		System.out.println("8 9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
