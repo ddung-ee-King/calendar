8fea405957ec225916c1aabe02828060dcb559cb
kjw08100 <tirkffus@naver.com>
2021-02-10 15:32:00 +0900

    update

--- src/Virtualcalendar3.java
+++ src/Virtualcalendar3.java
@@ -1,38 +1,35 @@
 import java.util.Scanner;
-public class Virtualcalendar2 {
+public class Virtualcalendar3 {
 	static final int[] array= {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	static final int[] leap_array= {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};//윤년
 	static final String PROMPT = "cal> ";
 	
-	public int Maxmonth(int a) {
-		return array[a-1];
+	public boolean Leapyear(int y) {
+		if(y%4==0 && y%100!=0 || y%400==0) {
+			return true;
+		}
+		return false;
 	}
 	
-	public int leap_Maxmonth(int a) {
-		return leap_array[a-1];
+	public int Maxmonth(int y, int m) {
+		if(Leapyear(y)) {
+			return leap_array[m-1];
+		}else {
+			return array[m-1];
+		}
 	}
 	
-	public void Virtual(int y, int a) {
-		System.out.printf("	<<%4d %3d>>\n", y, a);
+	public void Virtual(int y, int m) {
+		System.out.printf("      <<%d %2d>>\n", y, m);
 		System.out.println(" 일  월  화  수  목  금  토");
 		System.out.println(" ----------------------");
-		int Allday = Maxmonth(a);
-		int leap_Allday = leap_Maxmonth(a);
-		if(y%4==0 && y%100!=0 || y%400==0) {
-			for(int j = 1; j<=leap_Allday; j++) {
-				System.out.printf("%3d", j);
-				if(j%7==0) {
-					System.out.println();
-				}
+		int Allday = Maxmonth(y, m);
+		for(int i = 1; i <= Allday; i++) {
+			System.out.printf("%3d", i);
+			if(i%7==0) {
+				System.out.println();
 			}
-		}else {
-			for(int i = 1; i <= Allday; i++) {
-				System.out.printf("%3d", i);
-				if(i%7==0) {
-					System.out.println();
-				}
-			}	
-		}
+		}	
 //		System.out.println("1  2  3  4  5  6  7"); 
 //		System.out.println("8  9  10 11 12 13 14"); 
 //		System.out.println("15 16 17 18 19 20 21");
@@ -45,22 +42,22 @@ public class Virtualcalendar2 {
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Virtualcalendar2 v = new Virtualcalendar2();
-		int a, y;
+		Virtualcalendar3 v = new Virtualcalendar3();
+		int y, m;
 		while(true) {
 			System.out.println();
 			System.out.println("년도와 월을 입력하세요");
 			System.out.print(PROMPT);
 			y = sc.nextInt();//연도
-			a = sc.nextInt();//월
-			if(a<1) {
+			m = sc.nextInt();//월
+			if(m<1) {
 				System.out.println("월이 없습니다.");
 				break;
 			}
-			else if(a>12) {
+			else if(m>12) {
 				continue;
 			}
-			v.Virtual(y, a);
+			v.Virtual(y, m);
 			
 	}
 	}
